70faa4b4c19bb669eb05fb6f39fee461c52a42b6
Riley Williams <ririwilliams.rw@gmail.com>
2025-01-10 22:21:19 -0600

    Learned the basics of slash commands and env files

--- example_bot.py
+++ example_bot.py
@@ -1,20 +1,19 @@
 import discord
+from dotenv import load_dotenv
+import os
 
-intents = discord.Intents.default()
-intents.message_content = True
+load_dotenv()
 
-client = discord.Client(intents=intents)
+TOKEN = os.getenv("TOKEN")
 
-@client.event
+bot = discord.Bot()
+
+@bot.event
 async def on_ready():
-    print(f"We have logged in as {client.user}")
+    print(f"We have logged in as {bot.user}")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.slash_command(guild_ids=[1303445508840493156])
+async def hello(ctx: discord.ApplicationContext):
+    await ctx.respond("Hello!")
 
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
-
-client.run("MTMyNzQ2NjQ1MDU3ODk2NDU3Mg.GHyudm.zCrcmwUrevNhSCBQDwfw_RtIP2xe5l-ym_aktQ")
+bot.run(f"{TOKEN}")
