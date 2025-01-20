f1d6bb8deb122686551ed84d687ff9b326d4aa4f
Riley Williams <ririwilliams.rw@gmail.com>
2025-01-19 18:44:32 -0600

    Guess the Number game. Line 24 is returning an empty string for some reason

--- ping-bot.py
+++ ping-bot.py
@@ -1,3 +1,34 @@
+import discord
+from dotenv import load_dotenv
+import os
+import random
 
+load_dotenv()
+TOKEN = str(os.getenv("TOKEN"))
+
+bot = discord.Bot()
+
+
+
+@bot.event
+async def on_ready():
+    print(f"We have logged in as {bot.user}")
+
+@bot.slash_command(guild_ids=[1303445508840493156])
+async def hello(ctx: discord.ApplicationContext):
+    await ctx.respond("Hello!")
+
+@bot.slash_command(guild_ids=[1303445508840493156])
+async def gtn(ctx: discord.ApplicationContext):
+    await ctx.respond("Guess a number between 1 and 69")
+    guess = await bot.wait_for("message", check=lambda message: message.author == ctx.author)
+
+    if int(guess.content) == 5:
+        await ctx.send("You guessed it, hooray!")
+    else:
+        await ctx.send("NOPE, try again.")
+
+
+bot.run(f"{TOKEN}")
 
 
