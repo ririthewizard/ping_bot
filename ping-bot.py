5bed3b8bae3b8b9f3160476ccc39d0bdedc0ad95
Riley Williams <ririwilliams.rw@gmail.com>
2025-01-19 19:22:56 -0600

    Struggling to figure out why wait_for was returning an empty string

--- ping-bot.py
+++ ping-bot.py
@@ -1,12 +1,22 @@
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import os
 import random
 
 load_dotenv()
-TOKEN = str(os.getenv("TOKEN"))
+#TOKEN = os.getenv("TOKEN") TODO: Figure out if bot.run(os.getenv(f"{TOKEN})) ? bot.run(f"{TOKEN})
 
-bot = discord.Bot()
+description = """A bot that can ping you for events you create"""
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(
+    description = description,
+    intents = intents,
+) 
 
 
 
@@ -14,21 +24,24 @@ bot = discord.Bot()
 async def on_ready():
     print(f"We have logged in as {bot.user}")
 
-@bot.slash_command(guild_ids=[1303445508840493156])
-async def hello(ctx: discord.ApplicationContext):
-    await ctx.respond("Hello!")
+@bot.command(description="For when you wanna settle the score some other way")
+async def choose(ctx: commands.Context, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
 
-@bot.slash_command(guild_ids=[1303445508840493156])
-async def gtn(ctx: discord.ApplicationContext):
-    await ctx.respond("Guess a number between 1 and 69")
-    guess = await bot.wait_for("message", check=lambda message: message.author == ctx.author)
+#@bot.command()
+#async def gtn(ctx):
+#    """A Slash Command to play a Guess-the-Number game."""
+#
+#    await ctx.respond('Guess a number between 1 and 10.')
+#    guess = await bot.wait_for("message", check=lambda message: message.author == ctx.author)
+#
+#    if int(guess.content) == 5:
+#        await ctx.send('You guessed it!')
+#    else:
+#        await ctx.send('Nope, try again.')
 
-    if int(guess.content) == 5:
-        await ctx.send("You guessed it, hooray!")
-    else:
-        await ctx.send("NOPE, try again.")
 
-
-bot.run(f"{TOKEN}")
+bot.run(os.getenv("TOKEN"))
 
 
